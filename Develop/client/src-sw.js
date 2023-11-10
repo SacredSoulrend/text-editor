ef7950ca7423a3a26492491cf2a9699f6a23d6d6
Son Giang <sgiangt@gmail.com>
2023-11-09 19:54:40 -0700

    added asset caching in src-sw

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  // Use the CacheFirst strategy for asset caching
+  new CacheFirst({
+    // Cache name for assets
+    cacheName: 'assets-cache',
+    // Plugin to cache responses with a 200 status and ignore other statuses
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
